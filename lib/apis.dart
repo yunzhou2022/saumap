19919860cb883f1d364f7a9275b513e9c6ae180f
liyunzhou <liyunzhou2020@foxmail.com>
2021-02-23 14:37:52 +0800

    部署完成

--- lib/apis.dart
+++ lib/apis.dart
@@ -2,7 +2,8 @@ import 'package:dio/dio.dart';
 
 final dio = new Dio();
 
-final baseUrl = "http://192.168.57.1:8888";
+// final baseUrl = "http://192.168.57.1:8888";
+final baseUrl = "http://47.93.229.205:8888";
 
 final uploadImgUrl = baseUrl + "/api/location/uploadImg";
 
